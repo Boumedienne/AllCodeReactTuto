83289b1d0f45a47c87e9432af00b6a28df4c10e2
Boumedienne <cb_bouzidi@esi.dz>
2019-11-10 19:26:10 +0100

    If condiction in jsx expression

--- src/user.js
+++ src/user.js
@@ -5,3 +5,10 @@ import React from 'react';
 export function formatName(user){
     return user.firstname +'' +user.lastname;
 }
+
+export function getGreeting(user) {
+    if (user) {
+      return <h1>Hello, {formatName(user)}!</h1>;
+    }
+    return <h1>Hello, Stranger.</h1>;
+  }
